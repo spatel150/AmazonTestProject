08b44a61b556af21033b0e2c1cf247df5088eb4d
Samidh Patel <45617027+spatel150@users.noreply.github.com>
2023-08-30 16:56:46 -0400

    Update FileWriteTest.java

--- src/test/java/AdvancedSeleniumTests/FileWriteTest.java
+++ src/test/java/AdvancedSeleniumTests/FileWriteTest.java
@@ -1,2 +1,20 @@
-package AdvancedSeleniumTests;public class FileWriteTest {
+package AdvancedSeleniumTests;
+
+public class FileWriteTest {
+   public static void main(String[] args) throws IOException {
+        String path = "src/test/test-files/SampleData.xlsx";
+        FileInputStream fs = new FileInputStream(path);
+        Workbook wb = new XSSFWorkbook(fs);
+        Sheet sheet1 = wb.getSheetAt(1);
+        int lastRow = sheet1.getLastRowNum();
+        for (int i = 0; i <= lastRow; i++) {
+            Row row = sheet1.getRow(i);
+            Cell cell = row.createCell(2);
+            cell.setCellValue("99.99");
+        }
+
+        FileOutputStream fos = new FileOutputStream(path);
+        wb.write(fos);
+        fos.close();
+    }
 }
