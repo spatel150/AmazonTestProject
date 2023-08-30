ee6c5490da779a2d351507988b5f2b5fc161eedc
Samidh Patel <45617027+spatel150@users.noreply.github.com>
2023-08-30 16:56:16 -0400

    Update FileReaderTest.java

--- src/test/java/AdvancedSeleniumTests/FileReaderTest.java
+++ src/test/java/AdvancedSeleniumTests/FileReaderTest.java
@@ -1,2 +1,30 @@
-package AdvancedSeleniumTests;public class FileReaderTest {
+package AdvancedSeleniumTests;
+
+public class FileReaderTest {
+   public static void main(String[] args) throws IOException {
+
+        String excelFile = "src/test/test-files/SampleData.xlsx";
+
+        FileInputStream inputStream = new FileInputStream(excelFile);
+        Workbook workbook = new XSSFWorkbook(inputStream);
+
+        Sheet sheet1 = workbook.getSheetAt(1);
+
+        for (Row row : sheet1) {
+            for (Cell cell : row) {
+                String cellValue = "";
+                if (cell.getCellType() == CellType.STRING) {
+                    cellValue = cell.getStringCellValue();
+                } else if (cell.getCellType() == CellType.NUMERIC) {
+                    cellValue = String.valueOf(cell.getNumericCellValue());
+                }
+                System.out.print(cellValue + "\t");
+            }
+            System.out.println();
+        }
+
+        workbook.close();
+        inputStream.close();
+    }
+
 }
